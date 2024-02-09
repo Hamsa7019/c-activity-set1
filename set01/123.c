1fded422df31b565b2b3b0f119b7ed8841c45dc1
Manasa C S <manasac.bsc23@rvu.edu.in.com>
2024-02-09 15:56:16 +0530

    test

--- set01/123.c
+++ set01/123.c
@@ -9,7 +9,7 @@ void input_two_strings(char *string1,char *string2)
 int stringcompare(char *string1,char *string2)
 {
     int i=0;
-    for(i=0;string1[i]==string2[i] && string1[i]!='\0'&& string2[i]!='\0';i++);
+    for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
     if(string1[i]<string2[i])
     {
         return 1;
@@ -36,4 +36,13 @@ void output(char *string1,char *string2,int result)
         printf("both are equal");
     }
 
+}
+int main()
+{
+    int result;
+    char string1[100], string2[100];
+    input_two_strings(string1, string2);
+    result = stringcompare(string1, string2);
+    output(string1, string2, result);
+    return 0;
 }
