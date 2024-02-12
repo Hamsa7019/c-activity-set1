6a33d3993d79d7cc18932d18971d79ea40e8c5c2
Manasa C S <manasac.bsc23@rvu.edu.in.com>
2024-02-12 13:55:51 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,52 +1,40 @@
-#include <stdio.h>
-void input_two_strings(char *string1, char *string2)
+#include<stdio.h>
+char input_strings(char *string1, char *string2)
 {
-    printf("Enter the first string:\n");
-    scanf("%s", string1);
-    printf("Enter the second string:\n");
-    scanf("%s", string2);
+  printf("Enter the first string : \n");
+  scanf("%s", string1);
+  printf("Enter the second string : \n");
+  scanf("%s", string2);
+  return 0;
 }
 
-int stringcompare(char *string1, char *string2)
+char compare_the_strings(char *string1, char *string2)
 {
-    int i=0;
-    for ( i = 0; string1[i] == string2[i]; i++)
-    {
-        if (string1[i]=='\0')
-            return 0;
-    }
+  int i=0;
+  for (i=0 ; string1[i]!='\0' && string1[i]==string2[i] ; i++); 
+  return string1[i] - string2[i];
 
-    if (string1[i]>string2[i])
-    {
-        return 2;
-    }
-    else
-    {
-        return 1;
-    }
 }
-
 void output(char *string1, char *string2, int result)
 {
-    if (result == 0)
-    {
-        printf("%s is equal to %s\n", string1, string2);
-    }
-    else if (result == 2)
-    {
-        printf("%s is greater than %s\n", string1, string2);
-    }
-    else
-    {
-        printf("%s is greater than %s\n", string2, string1);
-    }
+  if(result > 0)
+  {
+    printf("%s is Greater than %s", string1,string2);
+  }
+  else if(result < 0)
+  {
+    printf("%s is Greater than %s", string2,string1);
+  }
+  else if(result == 0)
+  {
+    printf("%s is equal to %s",string1,string2);
+  }
 }
 int main()
 {
-    int result;
-    char string1[100], string2[100];
-    input_two_strings(string1, string2);
-    result = stringcompare(string1, string2);
-    output(string1, string2, result);
-    return 0;
+  char string1[100],string2[100];
+  input_strings(string1, string2);
+  int result = compare_the_strings(string1, string2);
+  output(string1, string2, result);
+  return 0;
 }
