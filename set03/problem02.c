443d27f09c5fc983fd868f053adb5b8860eb127d
Manasa C S <manasac.bsc23@rvu.edu.in.com>
2024-02-27 12:23:38 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -7,19 +7,18 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 #include<math.h>
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
 {
-    printf("enter the value for x1,y1\n");
+    printf("enter the value for x1 and y1\n");
     scanf("%f,%f",x1,y1);
-    printf("enter the value for x2,y2\n");
+    printf("enter the value for x2 and y2\n");
     scanf("%f,%f",x2,y2);
-    printf("enter the value for x3,y3\n");
+    printf("enter the value for x3 and y3\n");
     scanf("%f,%f",x3,y3);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-
-    float side1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    float side2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-    float side3 = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+    float side1 =(x1+x2-x3,y1+y2-y3);
+    float side2 =(x1+x3-x2,y1+y3-y2);
+    float side3 =(x2+x3-x1,y2+y3-y1);
     return (side1+side2-side3)&&(side2+side3-side1)&&(side3+side1-side2);
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
